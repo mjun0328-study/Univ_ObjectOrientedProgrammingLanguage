808f2643e9baa97483079156165c644a40dca35b
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2024-05-01 15:36:52 +0900

    Week2 Practice

--- Week2/Practice1/mile2kilo.java
+++ Week2/Practice1/mile2kilo.java
@@ -1,10 +1,10 @@
 class mile2kilo {
   public static void main(String[] args) {
     java.util.Scanner input = new java.util.Scanner(System.in);
-
+  
     System.out.print("Enter a miles: ");
-    double miles = ______;
-    double kilometers = ______;
-    System.out.println();
+    double miles = input.nextDouble();
+    double kilometers = Math.round(miles * 16) / 10.0;
+    System.out.println(miles + " miles is " + kilometers + " kilometers");
   }
 }
--- Week2/Practice2/multiply.java
+++ Week2/Practice2/multiply.java
@@ -3,30 +3,30 @@ class multiply {
     java.util.Scanner input = new java.util.Scanner(System.in);
     // Read a number
     System.out.print("Enter an integer between 0 and 1000: ");
-    int number = ______;
-
+    int number = input.nextInt();
+  
     // Find all digits in number
-    int lastDigit = ______;
-    int remainingNumber = ______;
-    int secondLastDigit = ______;
-    remainingNumber = ______;
-    int thirdLastDigit = ______;
-
+    int lastDigit = number % 10;
+    int remainingNumber = number / 10;
+    int secondLastDigit = remainingNumber % 10;
+    remainingNumber = remainingNumber / 10;
+    int thirdLastDigit = remainingNumber % 10;
+  
     // Protect from being zero
     if(lastDigit == 0) {
-      ______;
+      lastDigit = 1;
     }
     if(secondLastDigit == 0) {
-      ______;
-        }
-    if(thirdLastDigit == 0) {
-      ______;
+      secondLastDigit = 1;
     }
-
+    if(thirdLastDigit == 0) {
+      thirdLastDigit = 1;
+    }
+  
     // Obtain the multiply of all digits
     int multiply = lastDigit * secondLastDigit * thirdLastDigit;
-
+  
     // Display results
-    System.out.println(______);
+    System.out.println("The multiplication of all digits in " + number + " is " + multiply);
   }
 }
